a02bd8afd3e88157ce1c58f2387f2cf80b3df93e
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2025-10-30 17:48:47 +0000

    Deleta products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-const cart = [
+let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -33,4 +33,15 @@ function addToCart(productId) {
   }
 }
 
-export { cart, addToCart };
+function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+}
+
+export { cart, addToCart, removeFromCart };
