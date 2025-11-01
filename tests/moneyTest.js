c6e58045882c9e2e4c979a602a9aa0d70d0081a3
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2025-11-01 07:43:38 +0000

    create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("test suite : formatCurrency"); //naming a test group - test suite
+
+console.log("converts cents into dollars");
+if (formatCurrency(2095) === "20.95") {
+  //basic test case
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+console.log("works with zero");
+if (formatCurrency(0) === "0.00") {
+  //edge test case
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+console.log("works rounding up to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") {
+  //edge test case , rounding up
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+console.log("works rounding down to the nearest cent");
+if (formatCurrency(2000.4) === "20.00") {
+  //edge test case , rounding down
+  console.log("passed");
+} else {
+  console.log("failed");
+}
